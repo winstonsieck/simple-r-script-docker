2a4f036eca3a000a714a9bb3131076634c5422c3
Winston Sieck <41486655+winstonsieck@users.noreply.github.com>
2024-08-29 08:30:17 -0400

    add files to build r docker read json image

---
+++ r-docker-read-json/read_json.R
@@ -0,0 +1,18 @@
+# read data from json file, clean, and report status
+
+# load library
+library(rjson)
+
+# Read in messy data as a list 
+#   dat_list <- fromJSON(file = "r-docker-read-json/input/messy_data.json")
+dat_list <- fromJSON(file = "home/r-env/input/messy_data.json")
+
+# Clean list and convert to df
+dat_list <- lapply(dat_list, function(x) ifelse(x == "NULL" | x=="", NA, x))
+
+df <- do.call(rbind.data.frame, dat_list)
+
+if (exists("df") && is.data.frame(get("df"))) {
+  print("Success! - data frame ready")
+}
+
