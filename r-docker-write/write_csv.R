95c42006f7e43d6d9009a4d4ec6e0e7305e231b4
Winston Sieck <41486655+winstonsieck@users.noreply.github.com>
2024-08-30 12:43:38 -0400

    add files to build r image to write csv

---
+++ r-docker-write/write_csv.R
@@ -0,0 +1,11 @@
+# generate random data and save out as a csv
+
+# create data frame
+x <- rnorm(20,3,2)
+y <- x + rnorm(20,0,1)
+
+df <- cbind.data.frame(x,y)
+
+# save file as csv
+
+write.csv(df, "home/r-env/output/sample_data.csv", row.names=F)
